2e4e38febb388ad9a4c5e665c53729f413d1a427
moheebakhter <131138672+moheebakhter@users.noreply.github.com>
2025-08-19 13:13:19 +0500

    admin

--- mera_homework/myapp/models.py
+++ mera_homework/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Employee (models.Model):
+      name = models.CharField(max_length = 50)
+      salary =  models.CharField(max_length = 50)
+      designation =  models.CharField(max_length = 50)
+      department = models.CharField(max_length=50)
+      Qualification = models.CharField(max_length=50)
+
+      def __str__(self):
+          return self.name
+
 # Create your models here.
